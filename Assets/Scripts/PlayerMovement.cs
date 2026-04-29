2a993ebed77fb74c07bb830ae67384588942b0f6
= <aquinas>
2026-04-29 22:29:02 +0300

    Добавил движения

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
 	[SerializeField] private float moveSpeed = 5f;
 	private Rigidbody2D rb;
 	private Vector2 moveInput;
+	private Animator animator;
 	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>()
     }
 
     // Update is called once per frame
@@ -21,6 +23,17 @@ public class PlayerMovement : MonoBehaviour
     
     public void Move(InputAction.CallbackContext context)
     {
+	    animator.SetBool("isWalking", true);
+	    if (context.canceled)
+	    {
+		    animator.SetBool("isWalking", false);
+		    animator.SetFloat("LastInputX", moveInput.x);
+		    animator.SetFloat("LastInputY", moveInput.y);
+	    }
+	    
     	moveInput = context.ReadValue<Vector2>();
+
+	    animator.SetFloat("InputX", moveInput.x);
+	    animator.SetFloat("InputY", moveInput.y);
     }
 }
