4ba8dfea6db425caacebcf5116a182b29db8feff
= <aquinas>
2026-05-01 11:51:50 +0300

    Refactor MapTransition to include direction enum and update player position logic

--- Assets/Scripts/MapTransition.cs
+++ Assets/Scripts/MapTransition.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine.UIElements;
 
 public class MapTransition : MonoBehaviour
 {
     [SerializeField] private PolygonCollider2D mapBoundary;
     CinemachineConfiner2D confiner;
+    [SerializeField] private Direction direction;
 
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    
     private void Awake()
     {
         confiner = FindObjectOfType<CinemachineConfiner2D>();
@@ -16,6 +26,30 @@ public class MapTransition : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") && confiner != null)
         {
             confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
         }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+        
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += 2;
+                break;
+            case Direction.Down:
+                newPos.y -= 2;
+                break;
+            case Direction.Left:
+                newPos.x -= 2;
+                break;
+            case Direction.Right:
+                newPos.x += 2;
+                break;
+        }
+        
+        player.transform.position = newPos;
     }
 }
