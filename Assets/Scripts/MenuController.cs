70b12b56d41d0e8155c46e7d2b304d78f0e1774d
= <aquinas>
2026-05-01 15:37:08 +0300

    Add menu toggle functionality to MenuController
    - Introduced a public GameObject reference for the menu canvas.
    - Initialized the menu canvas to be inactive at the start.
    - Implemented toggle functionality for the menu canvas using the Tab key in the Update method.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject menuCanvas;
+    
     void Start()
     {
-        
+        menuCanvas.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
     }
 }
