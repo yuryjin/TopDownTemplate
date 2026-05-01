65151aaed94e3621e361c91c408ba37d58cc1d12
= <aquinas>
2026-05-01 17:47:11 +0300

    Add SaveController and SaveData classes for game save functionality

---
+++ Assets/Scripts/SaveController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class SaveController : MonoBehaviour
+{
+    private string saveLocation
+    
+    void Start()
+    {
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+
+        LoadGame();
+    }
+
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindObjectOfType<CinemachineConfiner>().BoundingShape2D.gameObject.name
+        };
+        
+        file.writeAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+            FindObjectOfType<CinemachineConfiner>().BoundingShape2D =
+                GameObject.Find(saveData.mapBoundary).GetComponent<PolygonCollider2D>();
+        }
+        else
+        {
+            SaveGame();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPosition;
+    public string mapBoundary;
+    
+}
