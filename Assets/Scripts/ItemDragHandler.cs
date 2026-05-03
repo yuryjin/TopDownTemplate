149bc754a2f1c4847b1b2e9b21afc952977b7fff
= <aquinas>
2026-05-03 18:06:17 +0300

    132

--- Assets/Scripts/ItemDragHandler.cs
+++ Assets/Scripts/ItemDragHandler.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemDragHandler : MonoBehaviour
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Transform originalParent;
+    CanvasGroup canvasGroup;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,50 @@ public class ItemDragHandler : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+        transform.SetParent(transform.root);
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+        
+        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+        Slot originalSlot = originalParent.GetComponent<Slot>();
+
+        if (dropSlot != null)
+        {
+            if (dropSlot.currentItem != null)
+            {
+                dropSlot.currentItem.transform.SetParent(originalParent.transform);
+                originalSlot.currentItem = dropSlot.currentItem;
+                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            }
+            else
+            {
+                dropSlot.currentItem = null;
+            }
+            
+            transform.SetParent(originalParent.transform);
+            dropSlot.currentItem = gameObject;
+        }
+        else
+        {
+            transform.SetParent(originalParent);
+        }
+        
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
     }
 }
