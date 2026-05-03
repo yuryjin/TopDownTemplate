d48b5b04f5ecd3fe31e21f1a3ca51ae4f3a6adca
= <aquinas>
2026-05-03 20:55:38 +0300

    Revert "Merge pull request #1 from yuryjin/add_collectibles"
    This reverts commit 77b4be73b1ec36c29114675b1093baf18eab26fa, reversing
    changes made to 50eada63e3ea49e764ddadbff0a2488b34849eea.

--- Assets/Scripts/FoodData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FoodData", menuName = "Inventory/Food Data")]
-public class FoodData : ScriptableObject
-{
-    public string foodName;
-    public Sprite sprite;
-}
